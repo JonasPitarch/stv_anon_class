c4cd444c1771814de1d8f6c043f35e06f4fcd0c1
jonpitsaf <jonpitsaf@jonpitsaf.com>
2024-09-12 11:52:41 +0200

    Instituto

--- Ej1/src/Main.java
+++ Ej1/src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Persona {
+    // ...
+}
+class Alumno extends Persona {
+
+}
+
+
 public class Main {
+
+    static void imprimirJerarquiaDeClasses(Class<?> c) {
+        System.out.print(c.getName() + (c.getSuperclass() != null ? "  >  " : "\n"));
+        if (c.getSuperclass() == null) return;
+        imprimirJerarquiaDeClasses(c.getSuperclass());
+    }
+
     public static void main(String[] args) {
 
+        Alumno alumno = new Alumno(){
+
+        };
+        java.lang.Object alumno2=new java.lang.Object(){
+
+        };
+        imprimirJerarquiaDeClasses(alumno.getClass());
+        System.out.println();
+        imprimirJerarquiaDeClasses(alumno2.getClass());
     }
 }
